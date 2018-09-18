7b93244a697e2bb310402833b2caf093b071cd11
Magnus Andersson <magnus08@gmail.com>
2018-09-18 09:47:57 +0200

    Added test endpoint

--- src/containers/Main.js
+++ src/containers/Main.js
@@ -3,14 +3,38 @@ import React, {Component} from 'react';
 import {BrowserRouter, Route, Switch} from 'react-router-dom';
 
 export class Main extends Component {
+
+  constructor(props) {
+    super(props);
+    this.state = {
+      msg: "...connecting"
+    };
+  }
+
+  componentDidMount() {
+    this._testEndpoint();
+  }
+
+  _testEndpoint = async () => {
+    try {
+      const res = await fetch(window.appState.config.apiUrls.blurp + "/participation/Oyvzt3dvtijkmidOSjn6/activity/340fb9b5-f318-49ea-be74-b14debf01312");
+      this.setState({msg: "alright"});
+      console.log("res = ", res);
+    } catch(e) {
+      this.setState({msg: "error: " + e});
+      console.log("error = ", e);
+    }
+  };
+
   render() {
+    const {msg} = this.state;
     return (
       <BrowserRouter basename={window.appContextPath}>
         <div>
           <Switch>
             <Route
               path="/"
-              render={() => <div>In the route</div>}
+              render={() => <div>{msg}</div>}
             />
           </Switch>
         </div>
